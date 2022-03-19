8192b9d5871db8c7331e5c63e1921a857a1d0921
Italo Canturil <65181776+ItaloCanturil@users.noreply.github.com>
2022-03-19 19:58:39 -0300

    Time: 88 ms (96.91%), Space: 45.6 MB (40.00%) - LeetHub

---
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function(nums) {
+  nums.sort((a, b) => a - b);
+let max = 0;
+for (let i = nums.length - 1; i >= 2; i--) {
+   let start = i - 2;
+   let end = i - 1;
+   while (start < end) {
+      if (nums[end] + nums[start] > nums[i]) {
+         return nums[end] + nums[start] + nums[i];
+      }
+      else {
+         start++;
+      };
+   };
+};
+return 0;
+};
